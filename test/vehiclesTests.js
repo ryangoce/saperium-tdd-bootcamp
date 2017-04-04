7f79d08a6228325973bf5e920950653ff30d22b2
Ryan Goce <ryangoce@gmail.com>
2017-04-04 08:02:07 +0800

    added boilerplate test

---
+++ test/vehiclesTests.js
@@ -0,0 +1,7 @@
+describe('Vehicle Tests:', function() {
+    describe('createVehicle', function() {
+        it('should not allow an empty vin', function() {
+            // TODO: Your tests go here
+        });
+    })
+});
